9c938b661f4909e4d4863ee70b72e58e4f4869da
moises-quezada <moisegeo2@gmail.com>
2026-05-18 14:51:26 -0600

    envio de carpeta con proyectos de python

---
+++ python/02 comentarios.py
@@ -0,0 +1,10 @@
+#esta es una linea de codigo
+print("que paso pisha")
+#esta wea pues dice ahi que paso pisha vea
+
+"""
+comentario multilinea
+que es un comentario multilinea
+
+"""
+#print("es un prin pero quiero ignorarla como ella ignoro lo que siento")
---
+++ python/03 variables.py
@@ -0,0 +1,18 @@
+x = "esta monda es una variable"
+
+print(x)
+
+X = "esta monda es otra variable"
+print(X)
+
+mivariable = "esta monda es una variable con un nombre mas largo"
+mi_variable = "esta monda es una variable con un nombre mas largo pero con guion bajo"
+_mi_variable = "esta monda es una variable con un nombre mas largo pero con guion bajo y un guion bajo al inicio"
+miVariable = "esta monda es una variable con un nombre mas largo pero con guion bajo y un guion bajo al inicio pero con camel case"
+mivariable2 = "esta monda es una variable con un nombre mas largo pero con guion bajo y un guion bajo al inicio pero con camel case y un numero al final"
+print(mivariable)
+print(mi_variable)
+print(_mi_variable)
+print(miVariable)
+print(mivariable2)
+
